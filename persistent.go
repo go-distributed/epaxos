dcccb2da9eb72cdf946a0b8009089cf8b434c8ca
Yifan Gu <guyifan1121@gmail.com>
2014-04-29 00:33:28 -0400

    WIP (persistent)
    add simple interfaces for persistent, using leveldb
    TODO: sync instance tomorrow

---
+++ persistent.go
@@ -0,0 +1,13 @@
+package epaxos
+
+type KVpair struct {
+	Key   string
+	Value []byte
+}
+
+type Persistent interface {
+	Put(key string, value []byte) error
+	Get(key string) ([]byte, error)
+	Delete(key string) error
+	BatchPut(kvs ...*KVpair) error
+}
---
+++ persistent/leveldb_test.go
@@ -0,0 +1,65 @@
+package persistent
+
+import (
+	"testing"
+
+	"code.google.com/p/leveldb-go/leveldb/db"
+	"github.com/go-distributed/epaxos"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewCloseAndDrop(t *testing.T) {
+	l, err := NewLevelDB("/tmp/test")
+	defer func() {
+		assert.NoError(t, l.Close())
+		assert.NoError(t, l.Drop())
+	}()
+
+	assert.NoError(t, err)
+	assert.NotNil(t, l)
+	assert.NotNil(t, l.ldb)
+	assert.Equal(t, l.wsync, &db.WriteOptions{Sync: true})
+}
+
+func TestPutAndGet(t *testing.T) {
+	l, err := NewLevelDB("/tmp/test")
+	assert.NoError(t, err)
+
+	defer func() {
+		assert.NoError(t, l.Close())
+		assert.NoError(t, l.Drop())
+	}()
+
+	assert.NoError(t, l.Put("hello", []byte("world")))
+
+	v, err := l.Get("hello")
+	assert.NoError(t, err)
+	assert.Equal(t, v, []byte("world"))
+}
+
+func TestBatchPut(t *testing.T) {
+	l, err := NewLevelDB("/tmp/test")
+	assert.NoError(t, err)
+
+	defer func() {
+		assert.NoError(t, l.Close())
+		assert.NoError(t, l.Drop())
+	}()
+
+	kv1 := &epaxos.KVpair{
+		Key:   "hello",
+		Value: []byte("world"),
+	}
+	kv2 := &epaxos.KVpair{
+		Key:   "epaxos",
+		Value: []byte("rocks"),
+	}
+
+	assert.NoError(t, l.BatchPut(kv1, kv2))
+	v, err := l.Get("hello")
+	assert.NoError(t, err)
+	assert.Equal(t, v, []byte("world"))
+
+	v, err = l.Get("epaxos")
+	assert.Equal(t, v, []byte("rocks"))
+}
